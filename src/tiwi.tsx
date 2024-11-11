3653efa5758d98a5186dbcd60e64ee27120ba4ec
Quentin Calvez <quentin.calvez@quentez.com>
2024-11-11 20:56:08 +0900

    Make library compatible with SSG.

--- src/tiwi.tsx
+++ src/tiwi.tsx
@@ -3,9 +3,7 @@ import {
   ComponentRef,
   ElementType,
   forwardRef,
-  useEffect,
   useMemo,
-  useRef,
 } from "react";
 import {twMerge} from "tailwind-merge";
 import {allIntrinsicElements} from "./elements.js";
@@ -27,56 +25,20 @@ import {
 // Variants helpers.
 //
 
-function areVariantsEqual<T extends string>(
-  variants1: TiwiVariantsProp<T>,
-  variants2: TiwiVariantsProp<T>
-) {
-  // False / Null / Undefined.
-  if (!variants1) {
-    return !variants2;
+function variantsToArray<T extends string>(variants: TiwiVariantsProp<T>) {
+  if (!variants) {
+    return [];
   }
 
-  // String.
-  if (typeof variants1 === "string") {
-    return variants1 === variants2;
+  if (typeof variants === "string") {
+    return [variants];
   }
 
-  // Array.
-  if (Array.isArray(variants1)) {
-    return (
-      Array.isArray(variants2) &&
-      variants1.length === variants2.length &&
-      variants1.every((v, i) => variants2[i] === v)
-    );
+  if (Array.isArray(variants)) {
+    return variants.filter(isString);
   }
 
-  // Map.
-  const keys1 = Object.keys(variants1);
-  const keys2 = variants2 ? Object.keys(variants2) : [];
-
-  return (
-    keys1.length === keys2.length &&
-    keys1.every(k => (variants2 as any)[k] === (variants1 as any)[k as T])
-  );
-}
-
-function useVariantsMemo<TValue, T extends string>(
-  memoFn: () => TValue,
-  variants: TiwiVariantsProp<T>
-): TValue {
-  const value = useRef<{variants: TiwiVariantsProp<T>; value: TValue}>();
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const result =
-    value.current && areVariantsEqual(value.current.variants, variants)
-      ? value.current
-      : {variants, value: memoFn()};
-
-  useEffect(() => {
-    value.current = result;
-  }, [result]);
-
-  return result.value;
+  return Object.keys(variants).filter(k => (variants as any)[k] === true);
 }
 
 //
@@ -116,23 +78,7 @@ const tiwiBase: TiwiFunction = <E extends ElementType>(
         const {className, variants, ...otherProps} =
           props as IntermediateProps<T>;
 
-        const requestedVariants = useVariantsMemo((): ReadonlySet<string> => {
-          if (!variants) {
-            return new Set();
-          }
-
-          if (typeof variants === "string") {
-            return new Set([variants]);
-          }
-
-          if (Array.isArray(variants)) {
-            return new Set(variants.filter(isString));
-          }
-
-          return new Set(
-            Object.keys(variants).filter(k => (variants as any)[k] === true)
-          );
-        }, variants);
+        const flatVariants = variantsToArray(variants);
 
         const mergedClassName = useMemo(() => {
           const allClassNames = classNames.reduce((list, current, index) => {
@@ -140,7 +86,7 @@ const tiwiBase: TiwiFunction = <E extends ElementType>(
 
             if (variantDefinition) {
               Object.keys(variantDefinition)
-                .filter(v => requestedVariants.has(v))
+                .filter(v => flatVariants.includes(v))
                 .forEach(v => {
                   const variantValue = variantDefinition[v as T];
                   if (!variantValue) {
@@ -159,7 +105,7 @@ const tiwiBase: TiwiFunction = <E extends ElementType>(
 
           // Refresh on parent variables to make fast-refresh work in RN.
           // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [classNames, variantDefinitions, className, requestedVariants]);
+        }, [classNames, variantDefinitions, className, ...flatVariants]);
 
         if (isTiwi) {
           return (
